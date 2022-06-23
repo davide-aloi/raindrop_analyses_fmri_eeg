627868d56418ea13bceb64cc3926af0435c1d406
Davi93 <dxa869@student.bham.ac.uk>
2022-06-23 15:09:17 +0100

    add comments

--- fmri/glm/first_level_glm_joystick.m
+++ fmri/glm/first_level_glm_joystick.m
@@ -1,16 +1,17 @@
-% GLM 1st level with implicit mask set to -inf
+% Davide Aloi - UOB
+% GLM 1st level for passive mobilisation or active task
+% just change the path to scans and onsets
 % p01 only
 
 clear all
 jobfile = {'C:\Users\davide\Documents\GitHub\raindrop_analyses_fmri_eeg\fmri\glm\first_level_glm_joystick_job.m'}; %directory of the .m file to run
 
 % path to folder with functional files, regr.txt file etc
-paths = {'D:\Raindrop_data\p01\p01_w06\day05\fmri_data\JOYSTICK_BASELINE_0005\nifti'
-         'D:\Raindrop_data\p01\p01_w06\day05\fmri_data\JOYSTICK_POST_0009\nifti\'};
+paths = {'D:\Raindrop_data\p01\p01_w06\day05\fmri_data\PASSIVE_MOV_DURING_TDCS_0008\nifti'};
 
 %onsets "move" for each scan
-onsets = {[35.7407513043727,115.793368579703,195.830565565208,275.855716173945,355.894188521779]
-          [35.6541913043475,115.685126028955,195.704172985512,275.731370666646,355.762838724651]};
+onsets = {[20.0025790144919,60.0120913623177,100.020758260869,140.030068405797,180.039398492752,220.048429449274,260.058399072463,300.065960811593,340.074130550724,380.083367420290,420.091604869565,460.100716057970,500.110689391304,540.118688927536,580.127433275362,620.135611826087,660.143200927536,700.151580753623,740.159270028986,780.166731594203,820.176934956522,860.184730434781,900.191994898551,940.200330666667,980.208153043477,1020.21792742029,1060.22598400000,1100.23384672464,1140.24248023188,1180.24961252174]
+          };
 
 nrun = length(paths);
 jobs = repmat(jobfile, 1, nrun);
