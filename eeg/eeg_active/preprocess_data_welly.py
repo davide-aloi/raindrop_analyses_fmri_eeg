65c22d3dd16223490244e334df2d1771a3bf6f6e
Davi93 <dxa869@student.bham.ac.uk>
2022-06-29 18:01:23 +0100

    I made good progress but I'm tired now

---
+++ eeg/eeg_active/preprocess_data_welly.py
@@ -0,0 +1,32 @@
+# Author Davide Aloi
+# Scripts for the analyses of the EEG data (active task) collected at the Wellington Hospital - Aloi Davide PhD UoB
+
+import numpy as np
+import mne
+import os
+import glob 
+import matplotlib.pyplot as plt
+from preprocessing.preprocess import preprocess 
+
+mne.set_log_level("WARNING")
+mne.cuda.init_cuda(verbose=True)
+mne.utils.set_config('MNE_USE_CUDA', 'true')  
+
+# Folders to the EEG active task data
+output_folder = 'D:\\Raindrop_eeg_analysis\\p01\\'
+sessions = ['D:\\Raindrop_data\\p01\\p01_w01\\eeg_baseline\\active_task\\', # baseline w 1
+            'D:\\Raindrop_data\\p01\\p01_w02\\day04_eeg\\active_task\\', # post w 2
+            'D:\\Raindrop_data\\p01\\p01_w03\\eeg_baseline\\active_task\\', # baseline w 3
+            'D:\\Raindrop_data\\p01\\p01_w04\\day04_eeg\\active_task\\', # post w 4
+            'D:\\Raindrop_data\\p01\\p01_w05\\eeg_baseline\\active_task\\', # baseline w5
+            'D:\\Raindrop_data\\p01\\p01_w06\\day04_eeg\\active_task\\'] # post w6
+
+# Paths to .mff files
+sessions_raw = []
+for n, session in enumerate(sessions):
+    sessions_raw.append(glob.glob(session + '\*.mff')[0])
+
+raw = mne.io.read_raw_egi(sessions_raw[0], preload=True)
+cd C:\Users\davide\Documents\GitHub\raindrop_analyses_fmri_eeg\eeg\eeg_active
+cleaned_epochs = preprocess('p01_w01','D:\\Raindrop_eeg_analysis\\p01\\', raw, 'GSN-HydroCel-128', 'STI 014',
+            events_id = dict(move=1,relax=2), save = True)
--- eeg/eeg_active/preprocessing/preprocess.py
+++ eeg/eeg_active/preprocessing/preprocess.py
@@ -1,8 +1,32 @@
 def preprocess(session_name, output_folder, raw, montage, stim_channel, events_id = dict(),
                 tmin = -.5, tmax = 4, filter = (1, 40), reject=dict(eeg=300e-6), baseline = None, save = False):
-                
-    # ADD DESCRIPTION
-    # ADD ARGUMENTS / OUTPUT 
+    """_summary_
+    EEG data preprocessing for ERD analysis. 
+    Steps: 
+        - definying montage
+        - applying low / high pass filters
+        - finding events / epoching
+        - bad electrodes detection using RANSAC (which are excluded from ICA)
+        - bad epochs detection (before ICA)
+
+
+    Args:
+        session_name (_type_): _description_
+        output_folder (_type_): _description_
+        raw (_type_): _description_
+        montage (_type_): _description_
+        stim_channel (_type_): _description_
+        events_id (_type_, optional): _description_. Defaults to dict().
+        tmin (float, optional): _description_. Defaults to -.5.
+        tmax (int, optional): _description_. Defaults to 4.
+        filter (tuple, optional): _description_. Defaults to (1, 40).
+        reject (_type_, optional): _description_. Defaults to dict(eeg=300e-6).
+        baseline (_type_, optional): _description_. Defaults to None.
+        save (bool, optional): _description_. Defaults to False.
+
+    Returns:
+        _type_: _description_
+    """
 
     # Workflow: autoreject -> ica -> autoreject
     # The workflow is suggested here 
@@ -18,50 +42,54 @@ def preprocess(session_name, output_folder, raw, montage, stim_channel, events_i
     import matplotlib.pyplot as plt
     import numpy as np
 
-    #print(raw.info)
+    # Specifying montage
     montage = mne.channels.make_standard_montage(montage)
- 
+
+
+    # Filtering Data
     raw_filtered = raw.copy().filter(filter[0], filter[1])
+
+
     # Reference on average
     raw_filtered.set_eeg_reference(ref_channels='average')
-
     picks_eeg = mne.pick_types(raw.info, meg=False, eeg=True, eog=True, stim=False,
                            exclude='bads')
 
+
     # Finding events
     events = mne.find_events(raw, stim_channel='STI 014', verbose=True) # or STIM
-    
     # Epoching data 
     epochs = mne.Epochs(raw_filtered, events, events_id, tmin, tmax, baseline=None,
                         reject=None, verbose=False, detrend=0, preload=True)
 
+
     # Bad channel detection (ransac)
+    from autoreject import Ransac  # this uses ransac from PREP
     print('Detecting bad channels with Ransac')
     print(time.strftime("%H:%M:%S", time.localtime()))
-    from autoreject import Ransac  # this uses ransac from PREP
-    ransac = Ransac(verbose=True, picks=picks_eeg, n_jobs=1)
+    ransac = Ransac(verbose=True, picks=picks_eeg, n_jobs=-1)
     epochs_clean = ransac.fit_transform(epochs)
     bad_chs = ransac.bad_chs_ # list with bad channels according to RANSAC 
     print('Bad channels detected: ' + str(len(bad_chs)))
     print(time.strftime("%H:%M:%S", time.localtime()))
     
+
     # Bad epochs rejection (autoreject before ICA)
     print('Bad epochs detection (autorject)')
     ar = autoreject.AutoReject(n_interpolate=[1, 2, 3, 4], random_state=11,
-                            n_jobs=1, verbose=True)
+                            n_jobs=-1, verbose=True)
     ar.fit(epochs[:20])  # fit on a few epochs to save time
     print(time.strftime("%H:%M:%S", time.localtime()))
     epochs_ar, reject_log = ar.transform(epochs, return_log=True)
-    reject_log.plot('horizontal')
-    plt.savefig(output_folder + session_name + '_autoreject_before_ica.jpg', dpi = 240)
-    plt.close()
+    reject_log.plot('horizontal', show = False)
+    plt.savefig(output_folder + session_name + '_autoreject_before_ica.jpg', dpi = 160)
+
     print(f'Rejected Epochs: {(reject_log.bad_epochs == True).sum()}')
     print(time.strftime("%H:%M:%S", time.localtime()))
 
-    # ICA without bad epochs / bad channels
+    # ICA without bad epochs / bad channelss
     epochs.info['bads'] = bad_chs
     print('Fitting ICA')
-    print(time.strftime("%H:%M:%S", time.localtime()))
     ica = mne.preprocessing.ICA(random_state=99)
     ica.fit(epochs[~reject_log.bad_epochs])
     print('Completed.')
@@ -71,36 +99,34 @@ def preprocess(session_name, output_folder, raw, montage, stim_channel, events_i
     print('Visually inspect results and select eye-blinks components...')
     ica.plot_components(picks = np.arange(0,30,1))
     print(ica.exclude) # list of components excluded
-    ica.plot_components(ica.exclude)
-    plt.savefig(output_folder + session_name + '_ica_bad_components.jpg', dpi = 240)
-    plt.close()
-    ica.plot_overlay(epochs.average(), exclude=ica.exclude)
-    plt.savefig(output_folder + session_name + '_ica_overlay.jpg', dpi = 240)
-    plt.close()
+    ica.plot_components(ica.exclude, show = False)
+    plt.savefig(output_folder + session_name + '_ica_bad_components.jpg', dpi = 160)
+    ica.plot_overlay(epochs.average(), exclude=ica.exclude, show = False)
+    plt.savefig(output_folder + session_name + '_ica_overlay.jpg', dpi = 160)
     ica.apply(epochs, exclude=ica.exclude)
     
     # Autoreject after high pass filter and ICA
     epochs.info['bads'] = []
     ar = autoreject.AutoReject(n_interpolate=[1, 2, 3, 4], random_state=11,
-                            n_jobs=1, verbose=True)
+                            n_jobs=-1, verbose=True)
     ar.fit(epochs[:20])  # fit on the first 20 epochs to save time
     epochs_ar, reject_log = ar.transform(epochs, return_log=True)
     epochs[reject_log.bad_epochs].plot(scalings=dict(eeg=100e-6))
-    reject_log.plot('horizontal')
-    plt.savefig(output_folder + session_name + '_autoreject_after_ica.jpg', dpi = 240)
-    plt.close()
+    reject_log.plot('horizontal', show = False)
+    plt.savefig(output_folder + session_name + '_autoreject_after_ica.jpg', dpi = 160)
 
     # We will visualize the cleaned average data and compare it against the bad segments.
     evoked_bad = epochs[reject_log.bad_epochs].average()
     plt.figure()
-    plt.plot(evoked_bad.times, evoked_bad.data.T * 1e6, 'r', zorder=-1)
-    epochs_ar.average().plot(axes=plt.gca())
-    plt.savefig(output_folder + session_name + '_epochs_ar_average_badseg.jpg', dpi = 240)
-
+    plt.plot(evoked_bad.times, evoked_bad.data.T * 1e6, 'r', zorder=-1, show = False)
+    epochs_ar.average().plot(axes=plt.gca(), show = False)
+    plt.savefig(output_folder + session_name + '_epochs_ar_average_badseg.jpg', dpi = 160)
+  
     # Re-Referencing after ICA
     mne.set_eeg_reference(epochs_ar, ref_channels='average', copy = False)
     epochs_ar.apply_baseline()
 
+    # Saving cleaned epochs
     if save == True:
         epochs_ar.save(output_folder + session_name + '_cleaned_epochs.mff' ,overwrite=True)
 
