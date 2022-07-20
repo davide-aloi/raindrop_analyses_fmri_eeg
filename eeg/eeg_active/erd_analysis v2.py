0f2027dc645804a3bf2119d7fe3649d2cdab4469
Davi93 <dxa869@student.bham.ac.uk>
2022-07-20 11:24:43 +0100

    erd analysis corrected (v2)

--- eeg/eeg_active/erd_analysis v2.py
+++ eeg/eeg_active/erd_analysis v2.py
@@ -4,13 +4,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.colors import TwoSlopeNorm
-import pandas as pd
-import seaborn as sns
 import mne
-from mne.datasets import eegbci
 from mne.io import concatenate_raws, read_raw_edf
 from mne.time_frequency import tfr_multitaper
-from mne.stats import permutation_cluster_1samp_test as pcluster_test
 import glob
 
 
@@ -19,16 +15,19 @@ epochs_cleaned_folder = 'D:\\Raindrop_eeg_analysis\\p01\\'
 sessions_raw = glob.glob(epochs_cleaned_folder + '*-epo.fif')
 event_ids = dict(move=1)
 freqs = np.arange(7, 31, 1) # mu and beta
+freqs_toplot = [(8,13),(13,30)]
+freqs_names = ['Mu','Beta']
 n_cycles = freqs / 2 
 time_bandwidth = 2.0
 tmin, tmax = -3.5, 4  #??? do not crop
 baseline = [-1, -0.5]  # baseline interval (in s) 
-vmin,vmax = -3.5, 5.5 # to avoid displaying edge artefact
-
+vmin,vmax = -4, 6 # to avoid displaying edge artefact reduce this (I think)
 cnorm = TwoSlopeNorm(vmin=vmin, vcenter=0, vmax=vmax)  # min, center & max ERDS
 kwargs = dict(n_permutations=100, step_down_p=0.05, seed=1,
-            buffer_size=None, out_type='mask')  # for cluster test
-sessions_raw = [sessions_raw[0]]
+            buffer_size=None, out_type='mask')
+
+#sessions_raw = [sessions_raw[0]] # if you want to debug on one session
+
 # Iterate each file in session_raw
 for session in sessions_raw:
     print(session)
@@ -40,19 +39,51 @@ for session in sessions_raw:
                         return_itc=False, average=False, decim=2,
                         time_bandwidth = time_bandwidth, n_jobs = -1)
 
-    tfr.crop(tmin, tmax).apply_baseline(baseline, mode="percent")
+    # cropping and applying baseline
+    tfr.crop(tmin, tmax).apply_baseline(baseline, mode="logratio")
     tfr_a = tfr.average() # averaging epochs
 
-    plt.figure()
-    f = 25
-    f = f - 7 # nevermind 
-    n_chs = len(tfr_a.info['ch_names'])
-    colors = plt.cm.Spectral_r(np.linspace(0, 1, n_chs))
+    # iterating each frequency range
+    for wf, fr in enumerate(freqs_toplot):
+        title = session.split('\\')[3].split('cleaned')[0].replace('_',' ')
+        # creating plot with 2 subpltos (montage, tfr)
+        fig, axes = plt.subplots(1, 2, figsize = (15,10), gridspec_kw={'width_ratios': [1, 2]})
+        axes[0].set_box_aspect(1)
+        axes[1].set_box_aspect(1/2)
+        axes[0].set_title('Montage')
+        axes[1].set_title('Session: ' + title + '\nBand: ' + freqs_names[wf] )
 
-    x = np.arange(0, tfr_a.data.shape[2], 1)
+        # defining band range
+        fmin, fmax = fr
+        fmin, fmax = fmin - freqs[0], fmax - freqs[0] # just because freq starts from 7
+        
+        # creating color map for lines and montage
+        n_chs = len(tfr_a.info['ch_names']) # number of channels
+        colors = plt.cm.Spectral_r(np.linspace(0, 1, n_chs))
 
-    for i in range(n_chs):
-        plt.plot(x, tfr_a.data[i, f, :], color = colors[i], linewidth = 0.8)
-    
-    plt.axvline(x = 500*(-tmin), color = 'black', linewidth = 0.4, label = 'onset')
-    plt.show()
+        x = np.arange(0, tfr_a.data.shape[2], 1)
+        # adding lines        
+        for i in range(n_chs):
+            # plotting the average across the frequencies selected, for each elect
+            data = np.mean(tfr_a.data[i,fmin:fmax,:], axis = 0)
+            axes[1].plot(x, data, color = colors[i], linewidth = 0.6, alpha = 0.8)
+
+        # plotting montage
+        cap = mne.channels.make_standard_montage('GSN-HydroCel-128')
+        for elidx, elect in enumerate(tfr.ch_names[:-1]):
+            pos = cap.get_positions()['ch_pos'][elect]
+            axes[0].plot(pos[0],pos[1], marker = '.', markersize = 5, color = colors[elidx])
+
+        # some final adjustments
+        axes[0].set_xticks([])
+        axes[0].set_yticks([])
+        # plotting onset line and setting xticks 
+        axes[1].axvline(x = 500*(-tmin), color = 'black', linewidth = 0.4, label = 'onset')
+        axes[1].set_xticks(ticks = [0,500,1000,1500,2000,2500,3000,3500], labels = np.arange(tmin,4.5,1))
+        axes[0].set_xlim(xmin = -0.1, xmax= 0.1)
+        fig.tight_layout()
+        plt.savefig(title.replace(' ','') + '_band_' + freqs_names[wf] +'.png', dpi = 300)
+        plt.close()
+
+
+
